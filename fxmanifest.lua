174a8f6edf9694dc6bad10bc6f576119b3497d0b
TheAnthonyLopez <anthonya46048@gmail.com>
2025-03-05 22:11:36 -0500

    fxmanifest.lua finished!

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'Priority Status Command (Discord Role-locked)'
+version '1.0'
+
+shared_script '@es_extended/imports.lua' -- If using ESX
+server_script 'server.lua'
+client_script 'client.lua'
